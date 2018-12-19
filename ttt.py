91ba5cedc2ffea69c4a9754518b11ec1e5984517
Bijan Fard <bfard@berkeley.edu>
2018-12-19 15:34:24 -0800

    Added easy, medium, and hard AI

--- ttt.py
+++ ttt.py
@@ -1,9 +1,23 @@
+import random
+import copy
 import sys
 
 def play(mode, symbol):
 	if mode not in ["human", "easy", "medium", "hard"]:
 		print('\nError: The given mode does not exist. Try "human", "easy", "medium", or "hard".')
 		exit()
+	elif mode == "human":
+		human_player = None
+	else:
+		if symbol == "X":
+			human_player = 1
+		elif symbol == "O":
+			human_player = -1
+		elif symbol is not None:
+			print('\nError: Invalid symbol choice. Try "X" or "O".')
+			exit()
+		else:
+			human_player = random.choice([-1, 1])
 
 	print("\nWelcome to Tic Tac Toe.")
 	print("To make a move, enter one of the following two-letter options.\n")
@@ -21,17 +35,18 @@ def play(mode, symbol):
 	board = [[0, 0, 0] for _ in range(3)]
 	move_num = 0
 	player_map = {1: "X", -1: "O"}
+	rand_probs = {"easy": 1, "medium": 0.5, "hard": 0}
 	square_map = {"UL": (0, 0), "UM": (0, 1), "UR": (0, 2), \
 				  "ML": (1, 0), "MM": (1, 1), "MR": (1, 2), \
 				  "LL": (2, 0), "LM": (2, 1), "LR": (2, 2)}
 
-	def get_winner():
-		sums = [sum(board[0]), sum(board[1]), sum(board[2])]
-		sums.append(board[0][0] + board[1][0] + board[2][0])
-		sums.append(board[0][1] + board[1][1] + board[2][1])
-		sums.append(board[0][2] + board[1][2] + board[2][2])
-		sums.append(board[0][0] + board[1][1] + board[2][2])
-		sums.append(board[0][2] + board[1][1] + board[2][0])
+	def get_winner(board_state):
+		sums = [sum(board_state[0]), sum(board_state[1]), sum(board_state[2])]
+		sums.append(board_state[0][0] + board_state[1][0] + board_state[2][0])
+		sums.append(board_state[0][1] + board_state[1][1] + board_state[2][1])
+		sums.append(board_state[0][2] + board_state[1][2] + board_state[2][2])
+		sums.append(board_state[0][0] + board_state[1][1] + board_state[2][2])
+		sums.append(board_state[0][2] + board_state[1][1] + board_state[2][0])
 		if 3 in sums:
 			return "X"
 		elif -3 in sums:
@@ -55,27 +70,96 @@ def play(mode, symbol):
 				layout += "\n-----------\n"
 		print(layout + "\n")
 
+	def move_random(player):
+		while True:
+			row = random.randint(0, 2)
+			col = random.randint(0, 2)
+			if board[row][col] == 0:
+				board[row][col] = player
+				break
+
+	def value(board_state, player, moves_played, alpha, beta):
+		winner = get_winner(board_state)
+		if winner is None and moves_played == 9:
+			return 0
+		elif winner == "X":
+			return 1
+		elif winner == "O":
+			return -1
+		elif moves_played % 2 == 0:
+			return max_value(board_state, player, moves_played, alpha, beta)
+		else:
+			return min_value(board_state, player, moves_played, alpha, beta)
+
+	def max_value(board_state, player, moves_played, alpha, beta):
+		max_val = -2
+		for next_state in successors(board_state, player):
+			max_val = max(max_val, value(next_state, -player, moves_played + 1, alpha, beta))
+			if max_val >= beta:
+				return max_val
+			alpha = max(max_val, alpha)
+		return max_val
+
+	def min_value(board_state, player, moves_played, alpha, beta):
+		min_val = 2
+		for next_state in successors(board_state, player):
+			min_val = min(min_val, value(next_state, -player, moves_played + 1, alpha, beta))
+			if min_val <= alpha:
+				return min_val
+			beta = min(min_val, beta)
+		return min_val
+
+	def successors(board_state, player):
+		successor_list = []
+		for row in range(3):
+			for col in range(3):
+				if board_state[row][col] == 0:
+					next_state = copy.deepcopy(board_state)
+					next_state[row][col] = player
+					successor_list.append(next_state)
+		return successor_list
+
+	def move_minimax(player):
+		nonlocal board
+		successor_list = successors(board, player)
+		values = [value(s, -player, move_num + 1, -2, 2) for s in successor_list]
+		if player == 1:
+			max_val = max(values)
+			options = [successor_list[i] for i in range(len(values)) if values[i] == max_val]
+		else:
+			min_val = min(values)
+			options = [successor_list[i] for i in range(len(values)) if values[i] == min_val]
+		board = random.choice(options)
+
 	def get_move():
 		if move_num % 2 == 0:
 			player = 1
 		else:
 			player = -1
-		while True:
-			move = input("Please enter a move for Player " + player_map[player] + ": ")
-			if move in square_map.keys():
-				row, col = square_map[move]
-				if board[row][col] == 0:
-					board[row][col] = player
-					break
-			print("\nInvalid move entered.\n")
+		if mode == "human" or player == human_player:
+			while True:
+				move = input("Please enter a move for Player " + player_map[player] + ": ")
+				if move in square_map.keys():
+					row, col = square_map[move]
+					if board[row][col] == 0:
+						board[row][col] = player
+						break
+				print("\nInvalid move entered.\n")
+		else:
+			print("The AI will make a move for Player " + player_map[player] + ".")
+			roll = random.uniform(0, 1)
+			if roll < rand_probs[mode]:
+				move_random(player)
+			else:
+				move_minimax(player)
 
-	while get_winner() is None and move_num < 9:
+	while get_winner(board) is None and move_num < 9:
 		print_board()
 		get_move()
 		move_num += 1
 
 	print_board()
-	winner = get_winner()
+	winner = get_winner(board)
 	if winner is None:
 		print("The game is a draw.")
 	else:
